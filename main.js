1988d6d9dd7fc0304f2746b640810e9182234d75
Carla1331 <carlataliaduartemartins123@gmail.com>
2024-10-23 11:30:35 -0300

    Adicionando modificacoes

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 const loginBtnlink = document.querySelector('.loginBtn-link');
-const recoverBtnlink = document.querySelector('.recoverBtn-link');
 const registerBtnlink = document.querySelector('.registerBtn-link');
-
+const recoverBtnlink = document.querySelector('.recoverBtn-link');
+const returnBtnlink = document.querySelector('.recoverReturn-link'); 
 
 const wrapper = document.querySelector('.wrapper');
 
-// Muda para a tela de cadastro
+
+loginBtnlink.addEventListener('click', () => {
+    wrapper.classList.remove('register-active', 'recover-active');
+    wrapper.classList.add('login-active');
+});
+
+
 registerBtnlink.addEventListener('click', () => {
     wrapper.classList.remove('login-active', 'recover-active');
     wrapper.classList.add('register-active');
 });
 
-// Muda para a tela de recuperação de senha
 recoverBtnlink.addEventListener('click', () => {
     wrapper.classList.remove('login-active', 'register-active');
     wrapper.classList.add('recover-active');
 });
 
-// Muda para a tela de login
-loginBtnlink.addEventListener('click', () => {
+returnBtnlink.addEventListener('click', () => {
     wrapper.classList.remove('register-active', 'recover-active');
     wrapper.classList.add('login-active');
 });
